439e74dc7e9df6464f63acbed727f9f455ab33c6
Donald Carr <sirspudd@gmail.com>
2016-02-23 14:30:53 -0800

    Start tracking all Arch repo infrastructure
    Change-Id: Ia939930c0cddf1a8a4656577f8ccf2156657b7f7

---
+++ local/setup_qpi.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+local_repo=${script_dir}/local
+
+sanity_check() {
+  if [[ "$(whoami)" != "root" ]]; then
+    echo "This needs to be run as root"
+    exit -1
+  fi
+ 
+  echo "Achtung! Chips! Heads! Fire! Compound fracture ahoy!"
+  echo "This script: enables root login/exposes your rootfs vs NFS!"
+  echo "" 
+  echo "This is gonna configure your device for development against the qt-sdk packages"
+  echo "I urge you to read the script before proceeding and accept no responsibility for the fallout"
+  echo "" 
+  echo "USE AT YOUR OWN RISK: Do you wish to proceed? [yespleaseonwards]"
+  
+  read i
+  if [[ "$i" != "yespleaseonwards" ]]; then
+    echo "Bailing for want of the magic phrase"
+    exit 0
+  fi
+}
+
+install_qpi_repo() {
+cat <<EOF >> /etc/pacman.conf
+
+[qpi]
+SigLevel = Optional
+Server = http://s3.amazonaws.com/spuddrepo/arch/\$arch
+EOF
+}
+
+setup_avahi() {
+  pacman -S avahi nss-mdns --noconfirm
+  
+  hostnamectl set-hostname qpi2
+  systemctl enable avahi-daemon
+}
+
+allow_root_login_ssh() {
+  echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+  systemctl restart sshd
+}
+
+setup_nfs() {
+# This is how I expose my sysroot to my main Arch machine
+# It is provided as the gcc sysroot argument
+
+# deliberately making it opt in
+
+  pacman -S nfs-utils --noconfirm
+  echo "/               *(rw,no_root_squash,fsid=0)" >> /etc/exports
+  systemctl enable nfs-server
+  exportfs -arv
+}
+
+sanity_check
+# Add our arch repo
+install_qpi_repo
+pacman -Sy
+pacman -S pi-compositor --noconfirm
+# we are relying on hostname advertizing via mdns
+setup_avahi
+# required for compilation of Qt libs on host
+setup_nfs
+# we are initially deploying as root from creator
+allow_root_login_ssh
+
+systemctl enable pi-compositor@root
---
+++ spudd_populate_repo.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+local_repo=${script_dir}/local
+
+cd ${local_repo}
+for dir in $(ls -d */); do
+  echo "Operating on $dir"
+  cd ${local_repo}/${dir}
+  for file in $(ls *.pkg.tar.xz); do
+    echo "Adding $file to $dir DB"
+    repo-add qpi.db.tar.gz $file 
+  done
+  cd ..
+done
+
+echo "Refreshed DBs; do you want to deploy to s3?"
+read i
+
+if [[ "$i" = "yes" ]]; then
+  deploy_dir="${script_dir}/s3/arch"
+  rm -Rf ${deploy_dir}
+  mkdir -p ${deploy_dir}
+  cp -rL ${local_repo}/* ${deploy_dir}
+fi
